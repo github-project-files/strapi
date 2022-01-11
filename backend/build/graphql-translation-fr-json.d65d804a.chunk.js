0154a0332dc818d310596139991d084b78a54e66
github-project-files <github.project.files@gmail.com>
2022-01-11 08:05:20 +0530

    strapi and next js project

---
+++ backend/build/graphql-translation-fr-json.d65d804a.chunk.js
@@ -0,0 +1,11 @@
+(self["webpackChunkbackend"] = self["webpackChunkbackend"] || []).push([[6804],{
+
+/***/ 96048:
+/***/ ((module) => {
+
+"use strict";
+module.exports = JSON.parse('{"plugin.description.long":"Crée un endpoint GraphQL supportant les méthodes par défaut de l\'API.","plugin.description.short":"Crée un endpoint GraphQL supportant les méthodes par défaut de l\'API."}');
+
+/***/ })
+
+}]);
---
+++ backend/build/graphql-translation-zh-Hans-json.d80ebe9b.chunk.js
@@ -0,0 +1,11 @@
+(self["webpackChunkbackend"] = self["webpackChunkbackend"] || []).push([[1989],{
+
+/***/ 8486:
+/***/ ((module) => {
+
+"use strict";
+module.exports = JSON.parse('{"plugin.description.long":"为默认API方法添加GraphQL端点","plugin.description.short":"为默认API方法添加GraphQL端点"}');
+
+/***/ })
+
+}]);
